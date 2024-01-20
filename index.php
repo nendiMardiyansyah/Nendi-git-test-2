ebc02f87321156f7db7deb037277d2ffefc2ac80
nendiMardiyansyah <newbieinone@gmail.com>
2024-01-21 00:31:05 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias asperiores adipisci nulla fugiat a illo eius, sit assumenda tempora qui quod necessitatibus nobis, provident architecto? Perferendis sed repudiandae fugiat architecto.</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
